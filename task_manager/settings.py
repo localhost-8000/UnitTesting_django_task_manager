a279ab8b588bf592d56d6e334215960b5278c2ab
Rahul Tiwari <rahultwr0005@gmail.com>
2022-02-20 14:55:56 +0530

    secret key updated

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = env("DJANGO_SECRET_KEY", default="django-insecure-hn4*^-v@9xjaxl&+-1u$hrl*d5wn^&j5_ir%18$-j2z@77u-g6")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
